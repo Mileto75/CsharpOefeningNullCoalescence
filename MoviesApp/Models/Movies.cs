ce3d0e89e3461b5393fa4f0b50e28a40e802ebd4
Mileto <mileto.di.marco@howest.be>
2019-09-03 15:12:27 +0200

    Add project files.

---
+++ MoviesApp/Models/Movies.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApp.Models
+{
+    class Movie
+    {
+        public int? movieCode { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public string director { get; set; }
+        public Movie sequel { get; set; }
+
+        public static List<Movie> GetMovies()
+        {
+            List<Movie> Movies = new List<Movie>();
+            Movies.Add(new Movie
+                    {
+                        movieCode = null,
+                        title = "Indiana Jones 1",
+                        director = null,
+                        sequel = new Movie
+                                {
+                                    movieCode=12,
+                                    title = "Indiana Jones 2",
+                                    genre = "Adventure",
+                                    director = null
+                                },
+                        genre = "Adventure"
+                    }
+            );
+            Movies.Add(null);
+            Movies.Add(new Movie
+            {
+                movieCode = 13,
+                title = "Avengers 1",
+                director = null,
+                sequel = null,
+                genre = "Adventure"
+            });
+            return Movies;
+
+        }
+        
+    }
+}
---
+++ MoviesApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using MoviesApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MoviesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a Key to Exit program");
+            List<string> Movies = new List<string>();
+            foreach (var movie in Movie.GetMovies())
+            {
+                Movies.Add(BuildMovieString(movie));
+            }
+            PrintMovies(Movies);
+            Console.ReadKey();
+        }
+        static void PrintMovies(List<string>Movies)
+        {
+            foreach(var Movie in Movies)
+            {
+                Console.WriteLine(Movie);
+            }
+        }
+        static string BuildMovieString(Movie movie)
+        {
+            string title = movie?.title ?? "<NoTitle>";
+            string director = movie?.director ?? "<NoDirector>";
+            int? movieCode = movie?.movieCode ?? 0;
+            string sequelTitle = movie?.sequel?.title ?? "<noSequel>";
+            return $"movieCode:{movieCode}\nTitle:{title}\nDirector:{director}\nSequel:{sequelTitle}\n";
+        }
+    }
+}
